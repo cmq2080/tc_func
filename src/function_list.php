6956a9c670f9a7014bfd2c3b875340a7f6707f3f
cmq2080 <1421224516@qq.com>
2020-06-14 21:16:04 +0800

    feat: 删除http_break_query函数，因为原生的parse_str函数功能与之几乎相同 (refs: #200614-2110)

--- src/function_list.php
+++ src/function_list.php
@@ -1,5 +1,5 @@
 <?php
-$files     = scandir('./functions'); // 扫描function目录
+$files = scandir(__DIR__ . '/functions'); // 扫描function目录
 $functions = [];
 
 foreach ($files as $key => $value) { // 结果去掉后缀名，加入到数组中（注意. 和.. ）
--- src/make_single_file.php
+++ src/make_single_file.php
@@ -6,15 +6,15 @@
  * Time: 09:48
  */
 
-require 'function_list.php';
+require_once 'function_list.php';
 
 $prefix = 'if (function_exists(\'#function#\') === false) {';
 $suffix = "}\n\n\n";
 
 $content = "<?php\n/**\n * Made at " . date('Y/m/d H:i:s') . "\n */\n\n";
 foreach ($functions as $function) {
-    $str    = file_get_contents('functions/' . $function . '.php');
-    $str    = substr_replace($str, '', 0, 5); // 去掉php文件头
+    $str = file_get_contents('functions/' . $function . '.php');
+    $str = substr_replace($str, '', 0, 5); // 去掉php文件头
     $prefix = str_replace('#function#', $function, $prefix); // 填充前缀方法名
 
     $content .= $prefix . $str . $suffix;
